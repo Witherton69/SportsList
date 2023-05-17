46ecda73c87cb1a468e46dbbc05e82f1af3cef32
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-05-17 08:50:51 -0500

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Katelyn Gosnell on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
